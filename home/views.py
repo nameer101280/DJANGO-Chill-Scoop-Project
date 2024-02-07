27c4ac96a6ff36002bd74ba87c650c0fdc74a847
Nameer Arshad <nameerarshad@hotmail.com>
2024-02-07 15:10:11 +0500

    Updated the Authentication and UI of web page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,7 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
         contact.save()
+        messages.success(request, "Your message has been sent!")
     
     
     return render(request, 'contact.html',)
